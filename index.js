c6798134532cdad61ecba6ad82d7cdb96c6a736f
Sushant Sharma <sushant20.sharma00@gmail.com>
2023-12-05 10:11:40 +0530

    changed port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
@@ -78,7 +79,7 @@ app.post("/api/persons", (req, res) => {
   res.json(newEntry);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
